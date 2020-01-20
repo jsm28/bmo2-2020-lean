237ac94427f67ac7f42a49e3b6af3775b05842df
Joseph Myers <jsm@polyomino.org.uk>
2020-01-20 19:11:35 +0000

    Clean up some "have" uses.

--- src/p1.lean
+++ src/p1.lean
@@ -34,7 +34,7 @@ begin
     { apply le_of_not_gt, rw gt, exact hn, },
     have h1nd : 1 ≤ n / b,
     { rw ← one_mul b at hnx, exact int.le_div_of_mul_le hbpos hnx, },
-    have hnd2: n / b < 2, by exact int.div_lt_of_lt_mul hbpos hnupper,
+    have hnd2: n / b < 2 := int.div_lt_of_lt_mul hbpos hnupper,
     have hnd1: n / b ≤ 1,
     { rw lt_iff_add_one_le at hnd2, exact le_of_add_le_add_right hnd2, },
     rw [ha, le_antisymm hnd1 h1nd],
@@ -45,7 +45,7 @@ end
 theorem mod_mul_2 (n : ℤ) (a : ℤ) (b : ℤ) (hbpos : 0 < b) (hmod : n % b = a) :
     n % (2 * b) = a ∨ n % (2 * b) = a + b :=
 begin
-  have hdvd: b ∣ 2 * b, by exact dvd_mul_left _ _,
+  have hdvd: b ∣ 2 * b := dvd_mul_left _ _,
   have hmodm: n % (2 * b) % b = a,
   { rw mod_mod_of_dvd n hdvd, exact hmod, },
   have h2bpos: 0 < 2 * b,
@@ -57,7 +57,7 @@ begin
     intro h2b0,
     rw h2b0 at h2bpos,
     exact lt_irrefl 0 h2bpos, },
-  have hn2bupper: n % (2 * b) < 2 * b, by exact mod_lt_of_pos _ h2bpos,
+  have hn2bupper: n % (2 * b) < 2 * b := mod_lt_of_pos _ h2bpos,
   exact mod_mul_2_aux (n % (2 * b)) a b hbpos hmodm hn2blower hn2bupper,
 end
 
@@ -72,7 +72,7 @@ pow_pos (show 0 < (2 : ℤ), by norm_num) k
 theorem induction_main (m : ℕ) (a : ℤ) (hm: 2 ≤ m) (ha : a % 2^m = 3)
     (hb: (p1_seq_next a) % 2^m = 3) : a % 2^(m+1) = 3 :=
 begin
-  have h2m1: (2^(m+1) : ℤ) = (2 * 2^m : ℤ), by refl,
+  have h2m1: (2^(m+1) : ℤ) = (2 * 2^m : ℤ) := rfl,
   rw h2m1,
   have hcases: a % (2 * 2^m) = 3 ∨ a % (2 * 2^m) = 3 + 2^m,
   { exact mod_mul_2 _ _ _ (power_of_2_pos m) ha },
@@ -82,7 +82,7 @@ begin
     unfold p1_seq_next at hb,
     rw mod_def at hc2,
     let d := a / (2 * 2^m),
-    have hd: d = a / (2 * 2^m), by refl,
+    have hd: d = a / (2 * 2^m) := rfl,
     rw ← hd at hc2,
     have hc2m: a = 3 + 2^m + 2 * 2^m * d,
     { rw ← hc2,
@@ -110,7 +110,7 @@ begin
       norm_num,
     end,
     have hm1: ∃ m1 : ℕ, m = 1 + m1,
-    { have hm2: ∃ m2 : ℕ, m = 2 + m2, by exact nat.exists_eq_add_of_le hm,
+    { have hm2: ∃ m2 : ℕ, m = 2 + m2 := nat.exists_eq_add_of_le hm,
       cases hm2 with m2 hm2_h,
       use 1 + m2,
       rw hm2_h,
@@ -125,7 +125,7 @@ begin
       skip,
       rw [hmm1, add_comm 1 m1, pow_succ (2 : ℤ) m1],
     end,
-    have hbb: (2 + 2 * 2 ^ m1 * (1 + 2 * d)) = 2 * (1 + 2^m1 * (1 + 2 * d)), by ring,
+    have hbb: (2 + 2 * 2 ^ m1 * (1 + 2 * d)) = 2 * (1 + 2^m1 * (1 + 2 * d)), {ring},
     rw [hbb, ← mul_assoc] at hb,
     conv at hb
     begin
@@ -163,7 +163,7 @@ begin
           add_comm m1 1, ← hmm1],
     end,
     rw [← add_assoc, mul_one, ← add_mod_mod, mod_self, add_zero] at hb,
-    have hbbx: (3 + (2 : ℤ)^m1) % 2^m = 3 % 2^m, by rw [← mod_mod, hb],
+    have hbbx: (3 + (2 : ℤ)^m1) % 2^m = 3 % 2^m, {rw [← mod_mod, hb]},
     rw [mod_eq_mod_iff_mod_sub_eq_zero, add_comm, add_sub_assoc, sub_self, add_zero,
         hmm1, add_comm, pow_succ] at hbbx,
     have hdvd: (2^m1 : ℤ) ∣ 2^m,
@@ -183,9 +183,9 @@ begin
         rw heq,
       end, },
     rw sub_self at heqx,
-    have heqxx: 2 * (2^m1 : ℤ) - 2^m1 = 2^m1, by ring,
+    have heqxx: 2 * (2^m1 : ℤ) - 2^m1 = 2^m1, {ring},
     rw heqxx at heqx,
-    have heqz: 0 < (2^m1 : ℤ), by exact power_of_2_pos m1,
+    have heqz: 0 < (2^m1 : ℤ) := power_of_2_pos m1,
     rw ← heqx at heqz,
     exact lt_irrefl (0 : ℤ) heqz, },
 end
@@ -202,7 +202,7 @@ begin
     unfold p1_seq_next at hb,
     rw mod_def at hc1,
     let d := a / (2 * 2),
-    have hd: d = a / (2 * 2), by refl,
+    have hd: d = a / (2 * 2) := rfl,
     rw ← hd at hc1,
     have hc1m: a = 1 + 2 * 2 * d,
     { conv
@@ -237,11 +237,11 @@ begin
   intro n,
   let an : ℤ := a n,
   let an1 : ℤ := a (n + 1),
-  have han1 : an1 = a (n + 1), by refl,
-  have hrecn : a (n + 1) = p1_seq_next an, by rw hrec n,
+  have han1 : an1 = a (n + 1) := rfl,
+  have hrecn : a (n + 1) = p1_seq_next an, {rw hrec n},
   rw ← han1 at hrecn,
-  have hanodd : odd an, by exact hodd n,
-  have han1odd : odd an1, by exact hodd (n + 1),
+  have hanodd : odd an := hodd n,
+  have han1odd : odd an1 := hodd (n + 1),
   rw hrecn at han1odd,
   exact induction_base an hanodd han1odd,
 end
@@ -254,11 +254,11 @@ begin
   intro n,
   let an : ℤ := a n,
   let an1 : ℤ := a (n + 1),
-  have han1 : an1 = a (n + 1), by refl,
-  have hrecn : a (n + 1) = p1_seq_next an, by rw hrec n,
+  have han1 : an1 = a (n + 1) := rfl,
+  have hrecn : a (n + 1) = p1_seq_next an, {rw hrec n},
   rw ← han1 at hrecn,
-  have hanmod : an % 2^m = 3, by exact hmod n,
-  have han1mod : an1 % 2^m = 3, by exact hmod (n + 1),
+  have hanmod : an % 2^m = 3 := hmod n,
+  have han1mod : an1 % 2^m = 3 := hmod (n + 1),
   rw hrecn at han1mod,
   exact induction_main m an hm hanmod han1mod,
 end
@@ -292,9 +292,9 @@ theorem all_powers_divides_terms_minus_three (a : ℕ → ℤ) (hodd : all_odd a
 begin
   intros m n,
   by_cases h2m : 2 ≤ m,
-  { have hmod : (a n) % 2^m = 3, by exact three_mod_all_powers a hodd hrec m h2m n,
+  { have hmod : (a n) % 2^m = 3 := three_mod_all_powers a hodd hrec m h2m n,
     rw mod_def at hmod,
-    have hmodx : a n - 2^m * (a n / 2^m) + 2^m * (a n / 2^m) = 3 + 2^m * (a n / 2 ^ m), by rw hmod,
+    have hmodx : a n - 2^m * (a n / 2^m) + 2^m * (a n / 2^m) = 3 + 2^m * (a n / 2 ^ m), {rw hmod},
     rw [add_comm, ← add_sub_assoc, add_comm, add_sub_assoc, sub_self,
         add_zero] at hmodx,
     rw [hmodx, add_comm, add_sub_assoc, sub_self, add_zero],
@@ -303,7 +303,7 @@ begin
     { apply le_of_not_gt,
       rw gt,
       exact h2m, },
-    have haodd: a n % 2 = 1, by exact hodd n,
+    have haodd: a n % 2 = 1 := hodd n,
     rw mod_def at haodd,
     have ha3m2: a n - 2 * (a n / 2) + 2 * (a n / 2) - 3 = 2 * (a n / 2) - 2,
     { rw [haodd, add_comm, add_sub_assoc],
@@ -318,10 +318,10 @@ begin
       rw (show (2 : ℤ) = 2 * 1, by norm_num),
     end,
     rw ← mul_sub_left_distrib at ha3m2,
-    have ha3d2 : 2 ∣ a n - 3, by exact dvd_of_mul_right_eq _ ha3m2.symm,
+    have ha3d2 : 2 ∣ a n - 3 := dvd_of_mul_right_eq _ ha3m2.symm,
     rw (show (2 : ℤ) = 2^1, by norm_num) at ha3d2,
     have hm1div: (2 : ℤ)^m ∣ 2^1,
-    { have hm1: ∃ m1 : ℕ, 1 = m + m1, by exact nat.exists_eq_add_of_le hmle1,
+    { have hm1: ∃ m1 : ℕ, 1 = m + m1 := nat.exists_eq_add_of_le hmle1,
       cases hm1 with m1 hm1_h,
       conv
       begin
@@ -341,15 +341,15 @@ theorem zero_if_all_powers_divide_nat (a : ℕ) (h: ∀ m : ℕ, 2^m ∣ a) : a 
 begin
   by_cases haeq0: a = 0,
   { assumption },
-  { have ha : 2^a ∣ a, by exact h a,
+  { have ha : 2^a ∣ a := h a,
     have h2pself : ∀ n : ℕ, n < 2^n,
     { intro n,
       exact nat.lt_pow_self (show 1 < 2, by norm_num) n, },
     exfalso,
-    have h2aself: a < 2^a, by exact h2pself a,
-    have hapos : a > 0, by exact nat.pos_of_ne_zero haeq0,
-    have hself2a : 2^a ≤ a, by exact (nat.le_of_dvd hapos) ha,
-    have haa: a < a, by exact lt_of_lt_of_le h2aself hself2a,
+    have h2aself: a < 2^a := h2pself a,
+    have hapos : a > 0 := nat.pos_of_ne_zero haeq0,
+    have hself2a : 2^a ≤ a := (nat.le_of_dvd hapos) ha,
+    have haa: a < a := lt_of_lt_of_le h2aself hself2a,
     exact lt_irrefl a haa },
 end
 
@@ -362,7 +362,7 @@ begin
   cases hb with b hb_h,
   have hbd : ∀ m : ℕ, 2^m ∣ b,
   { intro m,
-    have hma: 2^m ∣ a, by exact h m,
+    have hma: 2^m ∣ a := h m,
     rw hb_h at hma,
     norm_cast at hma,
     exact hma, },
@@ -381,7 +381,7 @@ begin
     exact all_powers_divides_terms_minus_three a hodd hrec m 0, },
   have ha0: 0 ≤ a 0 - 3,
   { linarith, },
-  have ha30: a 0 - 3 = 0, by exact zero_if_all_powers_divide_int (a 0 - 3) ha0 hallpowers,
+  have ha30: a 0 - 3 = 0 := zero_if_all_powers_divide_int (a 0 - 3) ha0 hallpowers,
   rw [← add_zero (3 : ℤ), ← ha30],
   ring,
 end
