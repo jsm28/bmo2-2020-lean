608ba3b1bd4f6b0d3f0f8ea4290f4f4772f7d213
Joseph Myers <jsm@polyomino.org.uk>
2020-04-25 01:25:13 +0000

    Some problem 3 cleanup.

--- src/p3_lemmas.lean
+++ src/p3_lemmas.lean
@@ -15,19 +15,19 @@ iff.rfl
 @[norm_cast] lemma coe_fin_le {n : ℕ} {a b : fin n} : (a : ℕ) ≤ (b : ℕ) ↔ a ≤ b :=
 iff.rfl
 
-/-- Converting an in-range number to `fin n` with `fin.of_nat`
+/-- Converting an in-range number to `fin (succ n)` with `fin.of_nat`
 produces a result whose value is the original number.  -/
-lemma of_nat_val {n : ℕ} {a : ℕ} (h : a < nat.succ n) :
+lemma of_nat_val_of_lt {n : ℕ} {a : ℕ} (h : a < nat.succ n) :
   ((fin.of_nat a) : fin (nat.succ n)).val = a :=
 nat.mod_eq_of_lt h
 
-/-- Converting the value of a `fin n` to `fin n` with `fin.of_nat`
+/-- Converting the value of a `fin (succ n)` to `fin (succ n)` with `fin.of_nat`
 produces a result whose value is the original number.  -/
 lemma of_nat_val_self {n : ℕ} (a : fin (nat.succ n)) :
   ((fin.of_nat a.val) : fin (nat.succ n)).val = a.val :=
-of_nat_val a.is_lt
+of_nat_val_of_lt a.is_lt
 
-/-- Converting the value of a `fin n` to `fin n` with `fin.of_nat`
+/-- Converting the value of a `fin (succ n)` to `fin (succ n)` with `fin.of_nat`
 results in the same value.  -/
 lemma of_nat_val_eq_self {n : ℕ} (a : fin (nat.succ n)) : fin.of_nat a.val = a :=
 begin
@@ -35,14 +35,14 @@ begin
   exact of_nat_val_self a
 end
 
-/-- Converting a `fin n` to `ℕ` and back with `fin.of_nat` results in
+/-- Converting a `fin (succ n)` to `ℕ` and back with `fin.of_nat` results in
 the same value. -/
 lemma of_nat_coe_eq_self {n : ℕ} (a : fin (nat.succ n)) : fin.of_nat (a : ℕ) = a :=
 of_nat_val_eq_self a
 
 /-- `fin.of_nat` of an in-range number, converted back to `ℕ`, is that
 number. -/
-lemma of_nat_coe {n : ℕ} {a : ℕ} (h : a < nat.succ n) :
+lemma of_nat_coe_of_lt {n : ℕ} {a : ℕ} (h : a < nat.succ n) :
   (((fin.of_nat a) : fin (nat.succ n)) : ℕ) = a :=
 nat.mod_eq_of_lt h
 
