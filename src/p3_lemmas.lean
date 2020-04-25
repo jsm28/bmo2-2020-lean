fde6a022dfd00a13fb7419e72be255600d1f66f5
Joseph Myers <jsm@polyomino.org.uk>
2020-04-25 15:52:19 +0000

    More problem 3 lemmas cleanup.

--- src/p3_lemmas.lean
+++ src/p3_lemmas.lean
@@ -22,29 +22,23 @@ lemma of_nat_val_of_lt {n : ℕ} {a : ℕ} (h : a < nat.succ n) :
 nat.mod_eq_of_lt h
 
 /-- Converting the value of a `fin (succ n)` to `fin (succ n)` with `fin.of_nat`
-produces a result whose value is the original number.  -/
-lemma of_nat_val_self {n : ℕ} (a : fin (nat.succ n)) :
-  ((fin.of_nat a.val) : fin (nat.succ n)).val = a.val :=
-of_nat_val_of_lt a.is_lt
-
-/-- Converting the value of a `fin (succ n)` to `fin (succ n)` with `fin.of_nat`
 results in the same value.  -/
 lemma of_nat_val_eq_self {n : ℕ} (a : fin (nat.succ n)) : fin.of_nat a.val = a :=
 begin
   rw fin.eq_iff_veq,
-  exact of_nat_val_self a
+  exact of_nat_val_of_lt a.is_lt
 end
-
-/-- Converting a `fin (succ n)` to `ℕ` and back with `fin.of_nat` results in
-the same value. -/
-lemma of_nat_coe_eq_self {n : ℕ} (a : fin (nat.succ n)) : fin.of_nat (a : ℕ) = a :=
-of_nat_val_eq_self a
 
 /-- `fin.of_nat` of an in-range number, converted back to `ℕ`, is that
 number. -/
 lemma of_nat_coe_of_lt {n : ℕ} {a : ℕ} (h : a < nat.succ n) :
   (((fin.of_nat a) : fin (nat.succ n)) : ℕ) = a :=
 nat.mod_eq_of_lt h
+
+/-- Converting a `fin (succ n)` to `ℕ` and back with `fin.of_nat` results in
+the same value. -/
+lemma of_nat_coe_eq_self {n : ℕ} (a : fin (nat.succ n)) : fin.of_nat (a : ℕ) = a :=
+of_nat_val_eq_self a
 
 /-- `fin.of_nat 0`, converted to `ℕ`, is 0. -/
 lemma of_nat_coe_zero {n : ℕ} : (((fin.of_nat 0) : fin (nat.succ n)) : ℕ) = 0 :=
