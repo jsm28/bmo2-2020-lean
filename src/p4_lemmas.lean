504fded8490f5d481e7a4b1df3f72ca3ec111ddd
Joseph Myers <jsm@polyomino.org.uk>
2020-04-29 01:50:35 +0000

    Update mathlib version used (more lemmas now in mathlib).

--- src/p4_lemmas.lean
+++ src/p4_lemmas.lean
@@ -3,20 +3,6 @@
 -- a more general form, or should any of them be?
 
 import data.real.cardinality
-import topology.instances.real
-
-open real
-
--- This is meaningful for general topological (semi)fields, not just ℝ
--- (so for complex and nnreal, for example), but mathlib does not have
--- topological fields or semifields.
-
-/-- Continuity at a point of the result of dividing two functions
-continuous at that point, where the denominator is nonzero. -/
-lemma continuous_at.div {α : Type} [topological_space α] {f : α → ℝ} {g : α → ℝ} {x : α}
-    (hf : continuous_at f x) (hg : continuous_at g x) (hnz : g x ≠ 0) :
-  continuous_at (λ x, f x / g x) x :=
-continuous_at.mul hf (continuous_at.comp (tendsto_inv hnz) hg)
 
 -- The following proofs that intervals of reals are uncountable are
 -- probably much more complicated than necessary.
