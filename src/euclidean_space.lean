4c2da7bdd61f488528d5ce86e3c07cb8dfb163f9
Joseph Myers <jsm@polyomino.org.uk>
2020-05-23 17:37:42 +0000

    Minor cleanups, and more simple lemmas about angles.

--- src/euclidean_space.lean
+++ src/euclidean_space.lean
@@ -1,6 +1,7 @@
 import algebra.big_operators
 import analysis.normed_space.real_inner_product
 import normed_add_torsor
+import tactic.apply_fun
 
 noncomputable theory
 open_locale big_operators
@@ -112,9 +113,172 @@ begin
     rw [abs_mul, abs_of_nonneg (norm_nonneg x), abs_of_nonneg (norm_nonneg y), mul_one] }
 end
 
+/-- The inner product of a vector with a multiple of itself. -/
+lemma inner_smul_self_left (x : V) (r : ℝ) : inner (r • x) x = r * (∥x∥ * ∥x∥) :=
+by rw [inner_smul_left, ←inner_self_eq_norm_square]
+
+/-- The inner product of a vector with a multiple of itself. -/
+lemma inner_smul_self_right (x : V) (r : ℝ) : inner x (r • x) = r * (∥x∥ * ∥x∥) :=
+by rw [inner_smul_right, ←inner_self_eq_norm_square]
+
+/-- The inner product of a nonzero vector with a nonzero multiple of
+itself, divided by the product of their norms, has absolute value
+1. -/
+lemma abs_inner_div_norm_mul_norm_eq_one_of_ne_zero_of_ne_zero_mul
+  (x : V) (r : ℝ) (hx : x ≠ 0) (hr : r ≠ 0) : abs (inner x (r • x) / (∥x∥ * ∥r • x∥)) = 1 :=
+begin
+  rw [inner_smul_self_right, norm_smul, real.norm_eq_abs, ←mul_assoc ∥x∥, mul_comm _ (abs r),
+      mul_assoc, abs_div, abs_mul r, abs_mul (abs r), abs_abs, div_self],
+  exact mul_ne_zero (λ h, hr (eq_zero_of_abs_eq_zero h))
+    (λ h, hx (norm_eq_zero.1 (eq_zero_of_mul_self_eq_zero (eq_zero_of_abs_eq_zero h))))
+end
+
+/-- The inner product of a nonzero vector with a positive multiple of
+itself, divided by the product of their norms, has value 1. -/
+lemma inner_div_norm_mul_norm_eq_one_of_ne_zero_of_pos_mul
+  (x : V) (r : ℝ) (hx : x ≠ 0) (hr : 0 < r) : inner x (r • x) / (∥x∥ * ∥r • x∥) = 1 :=
+begin
+  rw [inner_smul_self_right, norm_smul, real.norm_eq_abs, ←mul_assoc ∥x∥, mul_comm _ (abs r),
+      mul_assoc, abs_of_nonneg (le_of_lt hr), div_self],
+  exact mul_ne_zero (ne_of_gt hr)
+    (λ h, hx (norm_eq_zero.1 (eq_zero_of_mul_self_eq_zero h)))
+end
+
+/-- The inner product of a nonzero vector with a negative multiple of
+itself, divided by the product of their norms, has value -1. -/
+lemma inner_div_norm_mul_norm_eq_neg_one_of_ne_zero_of_neg_mul
+  (x : V) (r : ℝ) (hx : x ≠ 0) (hr : r < 0) : inner x (r • x) / (∥x∥ * ∥r • x∥) = -1 :=
+begin
+  rw [inner_smul_self_right, norm_smul, real.norm_eq_abs, ←mul_assoc ∥x∥, mul_comm _ (abs r),
+      mul_assoc, abs_of_neg hr, ←neg_mul_eq_neg_mul, div_neg_eq_neg_div, div_self],
+  exact mul_ne_zero (ne_of_lt hr)
+    (λ h, hx (norm_eq_zero.1 (eq_zero_of_mul_self_eq_zero h)))
+end
+
+/-- The inner product of two vectors, divided by the product of their
+norms, has absolute value 1 if and only if they are nonzero and one is
+a multiple of the other. One form of equality case for Cauchy-Schwarz. -/
+lemma abs_inner_div_norm_mul_norm_eq_one_iff (x y : V) :
+  abs (inner x y / (∥x∥ * ∥y∥)) = 1 ↔ (x ≠ 0 ∧ ∃ (r : ℝ), r ≠ 0 ∧ y = r • x) :=
+begin
+  split,
+  { intro h,
+    have hx0 : x ≠ 0,
+    { intro hx0,
+      rw [hx0, inner_zero_left, zero_div] at h,
+      norm_num at h,
+      exact h },
+    refine and.intro hx0 _,
+    set r := inner x y / (∥x∥ * ∥x∥) with hr,
+    use r,
+    set t := y - r • x with ht,
+    have ht0 : inner x t = 0,
+    { rw [ht, inner_sub_right, inner_smul_right, hr, ←inner_self_eq_norm_square,
+          div_mul_cancel _ (λ h, hx0 ((inner_self_eq_zero x).1 h)), sub_self] },
+    rw [←sub_add_cancel y (r • x), ←ht, inner_add_right, ht0, zero_add, inner_smul_right,
+        inner_self_eq_norm_square, ←mul_assoc, mul_comm,
+        mul_div_mul_left _ _ (λ h, hx0 (norm_eq_zero.1 h)), abs_div, abs_mul,
+        abs_of_nonneg (norm_nonneg _), abs_of_nonneg (norm_nonneg _), ←real.norm_eq_abs,
+        ←norm_smul] at h,
+    have hr0 : r ≠ 0,
+    { intro hr0,
+      rw [hr0, zero_smul, norm_zero, zero_div] at h,
+      norm_num at h },
+    refine and.intro hr0 _,
+    have h2 : ∥r • x∥ ^ 2 = ∥t + r • x∥ ^ 2,
+    { congr' 1,
+      refine eq_of_div_eq_one _ _ h,
+      intro h0,
+      rw [h0, div_zero] at h,
+      norm_num at h },
+    rw [pow_two, pow_two, ←inner_self_eq_norm_square, ←inner_self_eq_norm_square,
+        inner_add_add_self] at h2,
+    conv_rhs at h2 {
+      congr,
+      congr,
+      skip,
+      rw [inner_smul_right, inner_comm, ht0, mul_zero, mul_zero]
+    },
+    symmetry' at h2,
+    rw [add_zero, add_left_eq_self, inner_self_eq_zero] at h2,
+    rw h2 at ht,
+    exact eq_of_sub_eq_zero ht.symm },
+  { intro h,
+    rcases h with ⟨hx, ⟨r, ⟨hr, hy⟩⟩⟩,
+    rw hy,
+    exact abs_inner_div_norm_mul_norm_eq_one_of_ne_zero_of_ne_zero_mul x r hx hr }
+end
+
+/-- The inner product of two vectors, divided by the product of their
+norms, has value 1 if and only if they are nonzero and one is
+a positive multiple of the other. -/
+lemma inner_div_norm_mul_norm_eq_one_iff (x y : V) :
+  inner x y / (∥x∥ * ∥y∥) = 1 ↔ (x ≠ 0 ∧ ∃ (r : ℝ), 0 < r ∧ y = r • x) :=
+begin
+  split,
+  { intro h,
+    have ha := h,
+    apply_fun abs at ha,
+    norm_num at ha,
+    rcases (abs_inner_div_norm_mul_norm_eq_one_iff x y).1 ha with ⟨hx, ⟨r, ⟨hr, hy⟩⟩⟩,
+    use hx,
+    use r,
+    refine and.intro _ hy,
+    by_contradiction hrneg,
+    rw hy at h,
+    rw inner_div_norm_mul_norm_eq_neg_one_of_ne_zero_of_neg_mul x r hx
+      (lt_of_le_of_ne' (le_of_not_lt hrneg) hr) at h,
+    norm_num at h },
+  { intro h,
+    rcases h with ⟨hx, ⟨r, ⟨hr, hy⟩⟩⟩,
+    rw hy,
+    exact inner_div_norm_mul_norm_eq_one_of_ne_zero_of_pos_mul x r hx hr }
+end
+
+/-- The inner product of two vectors, divided by the product of their
+norms, has value -1 if and only if they are nonzero and one is
+a negative multiple of the other. -/
+lemma inner_div_norm_mul_norm_eq_neg_one_iff (x y : V) :
+  inner x y / (∥x∥ * ∥y∥) = -1 ↔ (x ≠ 0 ∧ ∃ (r : ℝ), r < 0 ∧ y = r • x) :=
+begin
+  split,
+  { intro h,
+    have ha := h,
+    apply_fun abs at ha,
+    norm_num at ha,
+    rcases (abs_inner_div_norm_mul_norm_eq_one_iff x y).1 ha with ⟨hx, ⟨r, ⟨hr, hy⟩⟩⟩,
+    use hx,
+    use r,
+    refine and.intro _ hy,
+    by_contradiction hrpos,
+    rw hy at h,
+    rw inner_div_norm_mul_norm_eq_one_of_ne_zero_of_pos_mul x r hx
+      (lt_of_le_of_ne' (le_of_not_lt hrpos) hr.symm) at h,
+    norm_num at h },
+  { intro h,
+    rcases h with ⟨hx, ⟨r, ⟨hr, hy⟩⟩⟩,
+    rw hy,
+    exact inner_div_norm_mul_norm_eq_neg_one_of_ne_zero_of_neg_mul x r hx hr }
+end
+
 /-- The undirected angle between two vectors. If either vector is 0,
 this is π/2. -/
 def angle_of_vectors (x y : V) : ℝ := (inner x y / (∥x∥ * ∥y∥)).arccos
+
+/-- The angle between two vectors does not depend on their order. -/
+lemma angle_of_vectors_comm (x y : V) : angle_of_vectors x y = angle_of_vectors y x :=
+begin
+  unfold angle_of_vectors,
+  rw [inner_comm, mul_comm]
+end
+
+/-- The angle between the negation of two vectors. -/
+@[simp] lemma angle_of_vectors_of_neg_of_neg (x y : V) :
+  angle_of_vectors (-x) (-y) = angle_of_vectors x y :=
+begin
+  unfold angle_of_vectors,
+  rw [inner_neg_neg, norm_neg, norm_neg]
+end
 
 /-- The angle between two vectors is nonnegative. -/
 lemma angle_of_vectors_nonneg (x y : V) : 0 ≤ angle_of_vectors x y :=
@@ -125,7 +289,7 @@ lemma angle_of_vectors_le_pi (x y : V) : angle_of_vectors x y ≤ real.pi :=
 real.arccos_le_pi _
 
 /-- The angle between a vector and the negation of another vector. -/
-lemma angle_of_vectors_of_neg (x y : V) :
+lemma angle_of_vectors_of_neg_right (x y : V) :
   angle_of_vectors x (-y) = real.pi - angle_of_vectors x y :=
 begin
   unfold angle_of_vectors,
@@ -133,6 +297,11 @@ begin
   congr,
   rw [norm_neg, inner_neg_right, neg_div]
 end
+
+/-- The angle between the negation of a vector and another vector. -/
+lemma angle_of_vectors_of_neg_left (x y : V) :
+  angle_of_vectors (-x) y = real.pi - angle_of_vectors x y :=
+by rw [←angle_of_vectors_of_neg_of_neg, neg_neg, angle_of_vectors_of_neg_right]
 
 /-- The cosine of the angle between two vectors. -/
 lemma cos_angle_of_vectors (x y : V) :
@@ -154,6 +323,42 @@ begin
     { rw norm_eq_zero at hy,
       rw [hy, inner_zero_right] } },
   { exact div_mul_cancel _ h }
+end
+
+/-- The angle between two vectors is zero if and only if they are
+nonzero and one is a positive multiple of the other. -/
+lemma angle_of_vectors_eq_zero_iff (x y : V) :
+  angle_of_vectors x y = 0 ↔ (x ≠ 0 ∧ ∃ (r : ℝ), 0 < r ∧ y = r • x) :=
+begin
+  unfold angle_of_vectors,
+  rw [←inner_div_norm_mul_norm_eq_one_iff, ←real.arccos_one],
+  split,
+  { intro h,
+    exact real.arccos_inj (abs_le.mp (abs_inner_div_norm_mul_norm_le_one x y)).1
+                      (abs_le.mp (abs_inner_div_norm_mul_norm_le_one x y)).2
+                      (by norm_num)
+                      (by norm_num)
+                      h },
+  { intro h,
+    rw h }
+end
+
+/-- The angle between two vectors is π if and only if they are nonzero
+and one is a negative multiple of the other. -/
+lemma angle_of_vectors_eq_pi_iff (x y : V) :
+  angle_of_vectors x y = real.pi ↔ (x ≠ 0 ∧ ∃ (r : ℝ), r < 0 ∧ y = r • x) :=
+begin
+  unfold angle_of_vectors,
+  rw [←inner_div_norm_mul_norm_eq_neg_one_iff, ←real.arccos_neg_one],
+  split,
+  { intro h,
+    exact real.arccos_inj (abs_le.mp (abs_inner_div_norm_mul_norm_le_one x y)).1
+                      (abs_le.mp (abs_inner_div_norm_mul_norm_le_one x y)).2
+                      (by norm_num)
+                      (by norm_num)
+                      h },
+  { intro h,
+    rw h }
 end
 
 /-- Two vectors have inner product 0 if and only if the angle between
@@ -221,14 +426,11 @@ end
 lemma norm_sub_square_eq_norm_square_add_norm_square_sub_two_mul_norm_mul_norm_mul_cos_angle
     (x y : V) :
   ∥x - y∥ * ∥x - y∥ = ∥x∥ * ∥x∥ + ∥y∥ * ∥y∥ - 2 * ∥x∥ * ∥y∥ * (angle_of_vectors x y).cos :=
-begin
-  rw [(show 2 * ∥x∥ * ∥y∥ * (angle_of_vectors x y).cos =
-            2 * ((angle_of_vectors x y).cos * (∥x∥ * ∥y∥)), by ring),
-      cos_angle_of_vectors_mul_norm_mul_norm, ←inner_self_eq_norm_square,
-      ←inner_self_eq_norm_square, ←inner_self_eq_norm_square, inner_sub_left, inner_sub_right,
-      inner_sub_right, inner_comm y x],
-  ring
-end
+by rw [(show 2 * ∥x∥ * ∥y∥ * (angle_of_vectors x y).cos =
+             2 * ((angle_of_vectors x y).cos * (∥x∥ * ∥y∥)), by ring),
+       cos_angle_of_vectors_mul_norm_mul_norm, ←inner_self_eq_norm_square,
+       ←inner_self_eq_norm_square, ←inner_self_eq_norm_square, inner_sub_sub_self,
+       sub_add_eq_add_sub]
 
 /-- Pons asinorum, vector angle form. -/
 lemma angle_sub_eq_angle_sub_rev_of_norm_eq (x y : V) (h : ∥x∥ = ∥y∥) :
@@ -259,6 +461,11 @@ def angle_of_points (p1 p2 p3 : P) : ℝ := angle_of_vectors (p1 -ᵥ p2 : V) (p
 
 notation `∠` := angle_of_points
 
+/-- The angle at a point does not depend on the order of the other two
+points. -/
+lemma angle_of_points_comm (p1 p2 p3 : P) : ∠ V p1 p2 p3 = ∠ V p3 p2 p1 :=
+angle_of_vectors_comm _ _
+
 /-- The angle at a point is nonnegative. -/
 lemma angle_of_points_nonneg (p1 p2 p3 : P) : 0 ≤ ∠ V p1 p2 p3 :=
 angle_of_vectors_nonneg _ _
@@ -273,8 +480,7 @@ lemma dist_square_eq_dist_square_add_dist_square_iff_angle_eq_pi_div_two (p1 p2 
     ∠ V p1 p2 p3 = real.pi / 2 :=
 by erw [metric_space.dist_comm p3 p2, norm_dist V p1 p3, norm_dist V p1 p2, norm_dist V p2 p3,
         ←norm_sub_square_eq_norm_square_add_norm_square_iff_angle_eq_pi_div_two,
-        vsub_sub_vsub_right_cancel V p1,
-        ←neg_vsub_eq_vsub_rev V p2 p3, norm_neg]
+        vsub_sub_vsub_right_cancel V p1, ←neg_vsub_eq_vsub_rev V p2 p3, norm_neg]
 
 /-- Law of cosines (cosine rule), angle-at-point form. -/
 lemma dist_square_eq_dist_square_add_dist_square_sub_two_mul_dist_mul_dist_mul_cos_angle
