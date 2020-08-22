4e9c934d2687ed028487cc1bc4b19fa077750c5a
Joseph Myers <jsm@polyomino.org.uk>
2020-08-22 00:50:34 +0000

    Expand framework for problem 2 solution.
    Easy direction of solution (that all sets as described do satisfy the
    conditions of the problem) expanded in terms of lemmas that should go
    in mathlib, so all "sorry"s are in proofs of those lemmas.  Harder
    direction still needs to be expanded like that, and all the "sorry"s
    eliminated.

--- src/p2.lean
+++ src/p2.lean
@@ -18,6 +18,79 @@ import geometry.euclidean
 
 import orthocenter
 
+namespace affine_subspace
+
+variables {k : Type*} {V : Type*} {P : Type*} [ring k] [add_comm_group V] [module k V]
+          [affine_space V P]
+include V
+
+-- Results to add to affine_space/basic.lean in mathlib.
+
+/-- A nonempty affine subspace is `⊤` if and only if its direction is
+`⊤`. -/
+@[simp] lemma direction_eq_top_iff_of_nonempty {s : affine_subspace k P}
+    (h : (s : set P).nonempty) :
+  s.direction = ⊤ ↔ s = ⊤ :=
+begin
+  split,
+  { intro hd,
+    rw ←direction_top k V P at hd,
+    refine ext_of_direction_eq hd _,
+    simp [h] },
+  { rintro rfl,
+    simp }
+end
+
+end affine_subspace
+
+section affine
+
+-- Results to add to affine_space/finite_dimensional.lean in mathlib.
+
+variables {k : Type*} {V : Type*} {P : Type*} [field k] [add_comm_group V] [module k V]
+          [affine_space V P]
+variables {ι : Type*}
+include V
+
+open finite_dimensional
+
+/-- The `vector_span` of a finite affinely independent family has
+dimension one less than its cardinality. -/
+lemma findim_vector_span_of_affine_independent [fintype ι] {p : ι → P}
+    (hi : affine_independent k p) {n : ℕ} (hc : fintype.card ι = n + 1) :
+  findim k (vector_span k (set.range p)) = n :=
+begin
+  sorry
+end
+
+/-- The `vector_span` of a finite affinely independent family whose
+cardinality is one more than that of the finite-dimensional space is
+`⊤`. -/
+lemma vector_span_eq_top_of_affine_independent_of_card_eq_findim_add_one [finite_dimensional k V]
+    [fintype ι] {p : ι → P} (hi : affine_independent k p)
+    (hc : fintype.card ι = findim k V + 1) :
+  vector_span k (set.range p) = ⊤ :=
+eq_top_of_findim_eq $ findim_vector_span_of_affine_independent hi hc
+
+/-- The `affine_span` of a finite affinely independent family whose
+cardinality is one more than that of the finite-dimensional space is
+`⊤`. -/
+lemma affine_span_eq_top_of_affine_independent_of_card_eq_findim_add_one [finite_dimensional k V]
+    [fintype ι] {p : ι → P} (hi : affine_independent k p)
+    (hc : fintype.card ι = findim k V + 1) :
+  affine_span k (set.range p) = ⊤ :=
+begin
+  have hn : (set.range p).nonempty,
+  { refine set.range_nonempty_iff_nonempty.2 (fintype.card_pos_iff.1 _),
+    simp [hc] },
+  rcases hn with ⟨p₁, hp₁⟩,
+  have hn : (affine_span k (set.range p) : set P).nonempty := ⟨p₁, mem_affine_span k hp₁⟩,
+  rw [←affine_subspace.direction_eq_top_iff_of_nonempty hn, direction_affine_span],
+  exact vector_span_eq_top_of_affine_independent_of_card_eq_findim_add_one hi hc
+end
+
+end affine
+
 open affine finite_dimensional
 
 variables {V : Type*} {P : Type*} [inner_product_space V] [metric_space P]
@@ -52,11 +125,40 @@ def concyclic (s : set P) : Prop :=
 
 include V
 
+-- Any three points in a concyclic set are affinely independent.
+-- Should go in mathlib in some form.
+lemma affine_independent_of_concyclic {s : set P} (hs : concyclic s) {p : fin 3 → P}
+    (hps : set.range p ⊆ s) (hpi : function.injective p) :
+  affine_independent ℝ p :=
+begin
+  sorry
+end
+
 -- A set of points forms an orthocentric system.  This should go in
 -- mathlib, along with various properties thereof.
 def orthocentric_system (s : set P) : Prop :=
 ∃ t : triangle ℝ P,
   t.orthocenter ∉ set.range t.points ∧ s = insert t.orthocenter (set.range t.points)
+
+-- Any three points in an orthocentric system are affinely
+-- independent.  This should go in mathlib.
+lemma affine_independent_of_orthocentric_system {s : set P} (ho : orthocentric_system s)
+    {p : fin 3 → P} (hps : set.range p ⊆ s) (hpi : function.injective p) :
+  affine_independent ℝ p :=
+begin
+  sorry
+end
+
+-- All triangles in an orthocentric system have the same circumradius.
+-- This should go in mathlib.
+lemma exists_circumradius_eq_of_orthocentric_system {s : set P} (ho : orthocentric_system s) :
+  ∃ r : ℝ, ∀ t : triangle ℝ P, set.range t.points ⊆ s → t.circumradius = r :=
+begin
+  rcases ho with ⟨t, hto, hts⟩,
+  use t.circumradius,
+  intros t₂ ht₂,
+  sorry
+end
 
 -- The description given as an answer to the problem.
 def p2_answer_desc (s : set P) : Prop :=
@@ -69,5 +171,25 @@ begin
   unfold p2_problem_desc p2_answer_desc,
   rw and.congr_right_iff,
   intro h4,
-  sorry
+  split,
+  { rintro ⟨hn3, r, hr⟩,
+    sorry },
+  { rintro (hc | ho),
+    { split,
+      { exact λ p hpi hps, affine_independent_of_concyclic hc hps hpi },
+      { rcases hc with ⟨c, r, hcr⟩,
+        use r,
+        intros t hts,
+        have ht : affine_span ℝ (set.range t.points) = ⊤,
+        { refine affine_span_eq_top_of_affine_independent_of_card_eq_findim_add_one
+            t.independent _,
+          simp [hd2] },
+        have hc : c ∈ affine_span ℝ (set.range t.points) :=
+          ht.symm ▸ affine_subspace.mem_top ℝ V c,
+        exact (t.eq_circumradius_of_dist_eq
+          hc
+          (λ i, hcr (t.points i) (hts (set.mem_range_self i)))).symm } },
+    { split,
+      { exact λ p hpi hps, affine_independent_of_orthocentric_system ho hps hpi },
+      { exact exists_circumradius_eq_of_orthocentric_system ho } } }
 end
