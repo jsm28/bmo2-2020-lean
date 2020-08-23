40eb1414ca3b30e202d40df7495c9a788f470a4c
Joseph Myers <jsm@polyomino.org.uk>
2020-08-23 21:50:55 +0000

    More on problem 2.
    Reducing to (a) various mathlib-appropriate geometrical lemmas, most
    of which still need to be proved, and (b) the fact that given a
    triangle in a set with the required properties, any point in the set
    not on its circumcircle must be the orthocentre.

--- src/p2.lean
+++ src/p2.lean
@@ -72,6 +72,24 @@ begin
     (λ i, hcr (s.points i) (hsps (set.mem_range_self i)))).symm
 end
 
+-- All n-simplices among concyclic points in n-space have the same
+-- circumcenter.  This should go in mathlib in some form.
+lemma exists_circumcenter_eq_of_concyclic {ps : set P} {n : ℕ} (hd : findim ℝ V = n)
+    (hc : concyclic ps) :
+  ∃ c : P, ∀ s : simplex ℝ P n, set.range s.points ⊆ ps → s.circumcenter = c :=
+begin
+  rcases hc with ⟨c, r, hcr⟩,
+  use c,
+  intros s hsps,
+  have hs : affine_span ℝ (set.range s.points) = ⊤,
+  { refine affine_span_eq_top_of_affine_independent_of_card_eq_findim_add_one s.independent _,
+    simp [hd] },
+  have hc : c ∈ affine_span ℝ (set.range s.points) := hs.symm ▸ affine_subspace.mem_top ℝ V c,
+  exact (s.eq_circumcenter_of_dist_eq
+    hc
+    (λ i, hcr (s.points i) (hsps (set.mem_range_self i)))).symm
+end
+
 -- Any three points in a concyclic set are affinely independent.
 -- Should go in mathlib in some form.
 lemma affine_independent_of_concyclic {s : set P} (hs : concyclic s) {p : fin 3 → P}
@@ -107,9 +125,57 @@ begin
   sorry
 end
 
+-- Given any triangle in an orthocentric system, the fourth point is
+-- its orthocenter.  This should go in mathlib.
+lemma eq_insert_orthocenter_of_orthocentric_system {s : set P} (ho : orthocentric_system s)
+    {t : triangle ℝ P} (ht : set.range t.points ⊆ s) :
+  s = insert t.orthocenter (set.range t.points) :=
+begin
+  sorry
+end
+
 -- The description given as an answer to the problem.
 def p2_answer_desc (s : set P) : Prop :=
 at_least_four_points s ∧ (concyclic s ∨ orthocentric_system s)
+
+-- Given three points in a set with no three collinear, pairwise
+-- unequal, they are affinely independent.
+theorem p2_affine_independent_of_ne {s : set P} (hn3 : no_three_points_collinear s)
+    {p1 p2 p3 : P} (h1 : p1 ∈ s) (h2 : p2 ∈ s) (h3 : p3 ∈ s) (h12 : p1 ≠ p2) (h13 : p1 ≠ p3)
+    (h23 : p2 ≠ p3) :
+  affine_independent ℝ ![p1, p2, p3] :=
+begin
+  have hi : function.injective ![p1, p2, p3],
+  { intros i1 i2 hi12,
+    fin_cases i1; fin_cases i2;
+      simpa [h12, h13, h23, h12.symm, h13.symm, h23.symm] using hi12 },
+  have hps : set.range ![p1, p2, p3] ⊆ s,
+  { rw set.range_subset_iff,
+    intro i,
+    fin_cases i; simp [h1, h2, h3] },
+  exact hn3 _ hi hps
+end
+
+-- Given three points in a set with no three collinear, pairwise
+-- unequal, the triangle with those as vertices.
+def p2_triangle_of_ne {s : set P} (hn3 : no_three_points_collinear s)
+    {p1 p2 p3 : P} (h1 : p1 ∈ s) (h2 : p2 ∈ s) (h3 : p3 ∈ s) (h12 : p1 ≠ p2) (h13 : p1 ≠ p3)
+    (h23 : p2 ≠ p3) :
+  triangle ℝ P :=
+⟨![p1, p2, p3], p2_affine_independent_of_ne hn3 h1 h2 h3 h12 h13 h23⟩
+
+-- Given three points in a set with no three collinear, pairwise
+-- unequal, the triangle with those as vertices has vertices in the
+-- set.
+lemma p2_triangle_of_ne_range_subset {s : set P} (hn3 : no_three_points_collinear s)
+    {p1 p2 p3 : P} (h1 : p1 ∈ s) (h2 : p2 ∈ s) (h3 : p3 ∈ s) (h12 : p1 ≠ p2) (h13 : p1 ≠ p3)
+    (h23 : p2 ≠ p3) :
+  set.range (p2_triangle_of_ne hn3 h1 h2 h3 h12 h13 h23).points ⊆ s :=
+begin
+  rw set.range_subset_iff,
+  intro i,
+  fin_cases i; simp [p2_triangle_of_ne, h1, h2, h3]
+end
 
 -- Any set with at least four points, no three collinear, contains a
 -- triangle.
@@ -144,23 +210,13 @@ begin
   rw [ne.def, subtype.ext_iff, subtype.coe_mk, subtype.coe_mk, ←ne.def] at h12,
   rw [ne.def, subtype.ext_iff, subtype.coe_mk, subtype.coe_mk, ←ne.def] at h23,
   rw [ne.def, subtype.ext_iff, subtype.coe_mk, subtype.coe_mk, ←ne.def] at h13,
-  let p : fin 3 → P := ![p1, p2, p3],
-  have hi : function.injective p,
-  { intros i1 i2 hi12,
-    fin_cases i1; fin_cases i2;
-      simpa [h12, h13, h23, h12.symm, h13.symm, h23.symm, p] using hi12 },
-  have hps : set.range p ⊆ s,
-  { refine set.subset.trans _ hs',
-    rw set.range_subset_iff,
-    intro i,
-    fin_cases i; simp [p, hp1s', hp2s', hp3s'] },
-  use ⟨p, hn3 p hi hps⟩,
-  exact hps
+  let t := p2_triangle_of_ne hn3 (hs' hp1s') (hs' hp2s') (hs' hp3s') h12 h13 h23,
+  use [t, p2_triangle_of_ne_range_subset hn3 (hs' hp1s') (hs' hp2s') (hs' hp3s') h12 h13 h23]
 end
 
 -- Given a triangle in a set with the properties of the problem, any
--- point in that set that is not one of the vertices of the triangle,
--- and not on its circumcircle, must be its orthocentre.
+-- point in that set that is not on the circumcircle of the triangle
+-- must be its orthocentre.
 theorem p2_eq_orthocentre {s : set P} (hd2 : findim ℝ V = 2) {t0 : triangle ℝ P} {p : P}
     (ht0s : set.range t0.points ⊆ s)
     (hr : ∀ (t : triangle ℝ P), set.range t.points ⊆ s → t.circumradius = t0.circumradius)
@@ -172,11 +228,7 @@ end
 
 -- Given p on the circumcircle of t0, not a vertex, not the
 -- orthocentre, the orthocentre not on the circumcircle; derive a
--- contradiction.  Consider a triangle t1 made of two of the vertices
--- of t0 plus the orthocentre of t0.  Then the two circumcircles meet
--- only at the two shared vertices, so p1 does not lie on the
--- circumcircle of t1, so must be its orthocentre, but that is the
--- third vertex of t0.
+-- contradiction.
 theorem p2_orthocentre_extra {s : set P} (hd2 : findim ℝ V = 2)
     (hn3 : no_three_points_collinear s) {t0 : triangle ℝ P} (ht0s : set.range t0.points ⊆ s)
     (hr : ∀ (t : triangle ℝ P), set.range t.points ⊆ s → t.circumradius = t0.circumradius)
@@ -185,7 +237,84 @@ theorem p2_orthocentre_extra {s : set P} (hd2 : findim ℝ V = 2)
     (hpr : dist p t0.circumcenter = t0.circumradius) :
   false :=
 begin
-  sorry
+  -- Consider a triangle t1 made of two of the vertices of t0 plus the
+  -- orthocentre of t0.
+  have h0s : ∀ (i : fin 3), t0.points i ∈ s,
+  { rwa set.range_subset_iff at ht0s },
+  have h0no : ∀ (i : fin 3), t0.points i ≠ t0.orthocenter,
+  { intros i h,
+    rw ←h at hor,
+    apply hor,
+    simp },
+  have h0np : ∀ (i : fin 3), t0.points i ≠ p,
+  { intros i h,
+    apply hpnt0,
+    rw set.mem_range,
+    exact ⟨i, h⟩ },
+  have h01 : t0.points 0 ≠ t0.points 1,
+  { intro h,
+    have hi := injective_of_affine_independent t0.independent h,
+    exact fin.ne_of_vne dec_trivial hi },
+  let t1 : triangle ℝ P := p2_triangle_of_ne hn3 (h0s 0) (h0s 1) hos h01 (h0no 0) (h0no 1),
+  have ht1s : set.range t1.points ⊆ s :=
+    p2_triangle_of_ne_range_subset hn3 (h0s 0) (h0s 1) hos h01 (h0no 0) (h0no 1),
+  -- Then the two circumcircles meet only at the two shared vertices,
+  -- so p1 does not lie on the circumcircle of t1, so must be its
+  -- orthocentre, but that is the third vertex of t0.
+  have hpt1 : dist p t1.circumcenter ≠ t1.circumradius,
+  { intro heq,
+    let t2 : triangle ℝ P := p2_triangle_of_ne hn3 (h0s 0) (h0s 1) hp h01 (h0np 0) (h0np 1),
+    have ht2s : set.range t2.points ⊆ s :=
+      p2_triangle_of_ne_range_subset hn3 (h0s 0) (h0s 1) hp h01 (h0np 0) (h0np 1),
+    have ht0span : affine_span ℝ (set.range t0.points) = ⊤,
+    { refine affine_span_eq_top_of_affine_independent_of_card_eq_findim_add_one t0.independent _,
+      simp [hd2] },
+    have ht1span : affine_span ℝ (set.range t1.points) = ⊤,
+    { refine affine_span_eq_top_of_affine_independent_of_card_eq_findim_add_one t1.independent _,
+      simp [hd2] },
+    have ht2span : affine_span ℝ (set.range t2.points) = ⊤,
+    { refine affine_span_eq_top_of_affine_independent_of_card_eq_findim_add_one t2.independent _,
+      simp [hd2] },
+    have ht02cc : t0.circumcenter = t2.circumcenter,
+    { have h : ∀ (i : fin 3), dist (t2.points i) t0.circumcenter = t0.circumradius,
+      { intro i,
+        fin_cases i; simp [t2, p2_triangle_of_ne, hpr] },
+      exact t2.eq_circumcenter_of_dist_eq (ht2span.symm ▸ affine_subspace.mem_top ℝ V _) h },
+    have ht12cc : t1.circumcenter = t2.circumcenter,
+    { have h : ∀ (i : fin 3), dist (t2.points i) t1.circumcenter = t1.circumradius,
+      { intro i,
+        fin_cases i,
+        { change dist (t1.points 0) _ = _,
+          simp },
+        { change dist (t1.points 1) _ = _,
+          simp },
+        { simp [t2, p2_triangle_of_ne, heq] } },
+      exact t2.eq_circumcenter_of_dist_eq (ht2span.symm ▸ affine_subspace.mem_top ℝ V _) h },
+    rw [ht02cc, ←ht12cc, ←hr t1 ht1s] at hor,
+    change dist (t1.points 2) _ ≠ _ at hor,
+    simpa using hor },
+  have hpt1o : p = t1.orthocenter,
+  { refine p2_eq_orthocentre hd2 ht1s _ hp hpt1,
+    intros t ht,
+    rw [hr t ht, hr t1 ht1s] },
+  let s' := insert t0.orthocenter (set.range t0.points),
+  have hs'o : orthocentric_system s',
+  { refine ⟨t0, _, rfl⟩,
+    intro h,
+    rw set.mem_range at h,
+    rcases h with ⟨i, hi⟩,
+    exact h0no i hi },
+  have ht1s' : set.range t1.points ⊆ s',
+  { rw set.range_subset_iff,
+    intro i,
+    fin_cases i; simp [t1, p2_triangle_of_ne] },
+  have hs' := eq_insert_orthocenter_of_orthocentric_system hs'o ht1s',
+  rw ←hpt1o at hs',
+  have hps' : p ∈ insert t0.orthocenter (set.range t0.points),
+  { change p ∈ s',
+    rw hs',
+    exact set.mem_insert _ _ },
+  simpa [hpno, h0np] using hps'
 end
 
 -- The main part of the solution: a set with the given property is as
